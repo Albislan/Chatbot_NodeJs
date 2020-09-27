37ac2f29ea73aef649d5e991d6233a6ef6810f0c
Albislan <albislan@hotmail.com>
2020-09-27 09:22:17 -0300

    Projeto já funcionando, respondendo uma mensagem simples, a cada mensagem recebida

---
+++ index.js
@@ -0,0 +1,11 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '1229062433:AAF2RFypzX1AW9LDRqX-J5EQH6ALzHk3ImE'; //token gerado pelo telegram Botfather//
+
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on('message', function (msg) {
+    const chatId = msg.chat.id;
+    console.log(msg.text);
+    bot.sendMessage(chatId, 'Obrigado por sua mensagem');
+});
